a668aa69fc1e4c75dcc97395d710cbcfabaa9d1c
Linda Zhao <lindajzhao@gmail.com>
2019-12-03 16:52:22 -0500

    scrapper working

--- scrapper/houseplants411.js
+++ scrapper/houseplants411.js
@@ -40,7 +40,6 @@ exports.__esModule = true;
 var cheerio = require("cheerio");
 var node_fetch_1 = require("node-fetch");
 var fs = require("fs");
-var str = "<div class=\"post-meta-value\">Amaryllis plants need  <a class=\"popUpMain\" target=\"\" href=\"https://www.houseplant411.com/glossary/light-for-houseplants-high\" title=\"bright indirect light\">bright indirect light<span class=\"popUp-outer\"><span class=\"popUpTop\"></span><span class=\"popUpMid\">Very few houseplants should be placed in direct sun. High light refers only to bright indirect light since direct sun often burns the leaves of indoor houseplants. An area that is too hot and dry encourages Spider Mites and causes blooms to quickly fade. A northern exposure really doesn't provide enough light for high light plants. These plants need to be placed directly in front of an east-facing window, within 1-3 feet of a west-facing window, and within 5 ft. of a south facing window. A high light area has over 300 ft. candles of light.</span><span class=\"popUpBottom\"></span></span></a>. Southern exposure is best, eastern and western exposures are adequate, northern exposure requires additional artificial light.</div>";
 // creates array of urls
 function getUrls() {
     return __awaiter(this, void 0, void 0, function () {
@@ -57,42 +56,23 @@ function getUrls() {
     });
 }
 function flattenTooltip(html, $) {
-    console.log(html);
     var tags = [];
-    //   const flat = html
-    //     .map(el => {
-    //       if (el.type === "text") {
-    //         console.log("____ text!", el);
-    //         return el.data;
-    //       } else {
-    //         console.log("____ else!", el);
-    //         // tags.push(el.attribs.title.trim());
-    //         // return el.attribs.title.trim();
-    //       }
-    //     })
-    //     .join("");
-    //   return {
-    //     text: flat,
-    //     tags
-    //   };
+    var flat = $.parseHTML(html)
+        .map(function (el) {
+        if (el.type === "text") {
+            return el.data;
+        }
+        else {
+            tags.push(el.attribs.title.trim());
+            return el.attribs.title.trim();
+        }
+    })
+        .join("");
+    return {
+        text: flat,
+        tags: tags
+    };
 }
-// function flattenTooltip(html, $) {
-//   const tags = [];
-//   const flat = $.parseHTML(html)
-//     .map(el => {
-//       if (el.type === "text") {
-//         return el.data;
-//       } else {
-//         tags.push(el.attribs.title.trim());
-//         return el.attribs.title.trim();
-//       }
-//     })
-//     .join("");
-//   return {
-//     text: flat,
-//     tags
-//   };
-// }
 function scrapePage(url) {
     return __awaiter(this, void 0, void 0, function () {
         var resp, html, $, commonName, scientificName, description, imageUrl, light, water, propagation;
@@ -114,7 +94,8 @@ function scrapePage(url) {
                     imageUrl = $(".popImage")[0].attribs.href;
                     light = $(".post-meta")
                         .children()
-                        .slice(1, 2);
+                        .eq(1)
+                        .html();
                     water = $(".post-meta")
                         .children()
                         .eq(3)
@@ -124,15 +105,17 @@ function scrapePage(url) {
                         .eq(23)
                         .html();
                     return [2 /*return*/, {
+                            url: url,
                             profile: {
                                 commonName: commonName,
                                 scientificName: scientificName,
-                                imageUrl: imageUrl
+                                imageUrl: imageUrl,
+                                description: description
                             },
                             care: {
                                 light: flattenTooltip(light, $),
-                                water: water,
-                                propagation: propagation
+                                water: flattenTooltip(water, $),
+                                propagation: flattenTooltip(propagation, $)
                             }
                         }];
             }
@@ -147,9 +130,10 @@ function scrapeHouseplants411() {
                 case 0: return [4 /*yield*/, getUrls()];
                 case 1:
                     urls = _a.sent();
-                    return [4 /*yield*/, scrapePage(urls[5])];
+                    return [4 /*yield*/, scrapePage(urls[10])];
                 case 2:
                     plantModel = _a.sent();
+                    console.log("___ scraped obj", plantModel);
                     return [2 /*return*/];
             }
         });
--- scrapper/houseplants411.ts
+++ scrapper/houseplants411.ts
@@ -3,8 +3,6 @@ import * as cheerio from "cheerio";
 import fetch from "node-fetch";
 import * as fs from "fs";
 
-const str = `<div class="post-meta-value">Amaryllis plants need  <a class="popUpMain" target="" href="https://www.houseplant411.com/glossary/light-for-houseplants-high" title="bright indirect light">bright indirect light<span class="popUp-outer"><span class="popUpTop"></span><span class="popUpMid">Very few houseplants should be placed in direct sun. High light refers only to bright indirect light since direct sun often burns the leaves of indoor houseplants. An area that is too hot and dry encourages Spider Mites and causes blooms to quickly fade. A northern exposure really doesn't provide enough light for high light plants. These plants need to be placed directly in front of an east-facing window, within 1-3 feet of a west-facing window, and within 5 ft. of a south facing window. A high light area has over 300 ft. candles of light.</span><span class="popUpBottom"></span></span></a>. Southern exposure is best, eastern and western exposures are adequate, northern exposure requires additional artificial light.</div>`;
-
 // creates array of urls
 async function getUrls() {
   //   const resp = await fetch("https://www.houseplant411.com/houseplant?popup=2");
@@ -21,45 +19,23 @@ async function getUrls() {
 }
 
 function flattenTooltip(html, $) {
-  console.log(html);
   const tags = [];
-  //   const flat = html
-  //     .map(el => {
-  //       if (el.type === "text") {
-  //         console.log("____ text!", el);
-  //         return el.data;
-  //       } else {
-  //         console.log("____ else!", el);
-  //         // tags.push(el.attribs.title.trim());
-  //         // return el.attribs.title.trim();
-  //       }
-  //     })
-  //     .join("");
+  const flat = $.parseHTML(html)
+    .map(el => {
+      if (el.type === "text") {
+        return el.data;
+      } else {
+        tags.push(el.attribs.title.trim());
+        return el.attribs.title.trim();
+      }
+    })
+    .join("");
 
-  //   return {
-  //     text: flat,
-  //     tags
-  //   };
+  return {
+    text: flat,
+    tags
+  };
 }
-
-// function flattenTooltip(html, $) {
-//   const tags = [];
-//   const flat = $.parseHTML(html)
-//     .map(el => {
-//       if (el.type === "text") {
-//         return el.data;
-//       } else {
-//         tags.push(el.attribs.title.trim());
-//         return el.attribs.title.trim();
-//       }
-//     })
-//     .join("");
-
-//   return {
-//     text: flat,
-//     tags
-//   };
-// }
 
 async function scrapePage(url: string) {
   const resp = await fetch(url);
@@ -76,8 +52,8 @@ async function scrapePage(url: string) {
 
   const light = $(".post-meta")
     .children()
-    .slice(1, 2);
-  // .eq(1);
+    .eq(1)
+    .html();
 
   const water = $(".post-meta")
     .children()
@@ -90,24 +66,26 @@ async function scrapePage(url: string) {
     .html();
 
   return {
+    url,
     profile: {
       commonName,
       scientificName,
-      imageUrl
+      imageUrl,
+      description
     },
     care: {
       light: flattenTooltip(light, $),
-      water,
-      propagation
+      water: flattenTooltip(water, $),
+      propagation: flattenTooltip(propagation, $)
     }
   };
 }
 
 async function scrapeHouseplants411() {
   const urls = await getUrls();
-  const plantModel = await scrapePage(urls[5]);
+  const plantModel = await scrapePage(urls[10]);
 
-  //   console.log("___ scraped obj", plantModel.care.light);
+  console.log("___ scraped obj", plantModel);
 }
 
 scrapeHouseplants411();
